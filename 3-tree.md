6c64740165850a66fa59bc35c6446a624f5cd46d
OWBrony <97199545+OWBrony@users.noreply.github.com>
2022-07-06 09:57:59 -0600

    More progress on Tree stuff
    I added to the Tree readme and completed the solution for the Tree code.

---
+++ 3-tree.md
@@ -0,0 +1,8 @@
+* Throughout your travels in the realm you
+* have discovered many items of value to you.
+* However, you detirmine that it would be easier
+* let your computer handle your inventory instead
+* of writing it down.
+
+* Your job is to finish constructing the tree structure
+* that will act as your inventory.
--- tree_solution.py
+++ tree_solution.py
@@ -3,8 +3,8 @@ class tree():
         def __init__(self,key,name):
             self.left = None
             self.right = None
-            self.key = None
-            self.name = None
+            self.key = key
+            self.name = name
     
     def __init__(self):
         self.root = None
@@ -31,10 +31,69 @@ class tree():
                 node.right = tree.node(key, name)
             else:
                 if key != node.right.key:
-                    self._insert(key, name, node)
+                    self._insert(key, name, node.right)
     
-    def read_tree(self,node):
+    def __iter__(self):
+        """Do not touch this one"""
+        yield from self._read_tree(self.root)
+
+    def _read_tree(self,node):
         if node is not None:
-            yield from self.read_tree(node.left)
+            yield from self._read_tree(node.left)
             yield node.name
-            yield from self.read_tree(node.right)
+            yield from self._read_tree(node.right)
+
+    def __reversed__(self):
+        """Do not touch this one"""
+        yield from self._read_reverse_tree(self.root)
+
+    def _read_reverse_tree(self,node):
+        if node is not None:
+            yield from self._read_reverse_tree(node.right)
+            yield node.name
+            yield from self._read_reverse_tree(node.left)
+
+    def search_tree(self,key,node):
+        if key == node.key:
+            return True
+        elif key < node.key:
+            if node.left is not None:
+                if self.search_tree(name,node.left):
+                    return True
+                else:
+                    return False
+        elif key > node.key:
+            if node.right is not None:
+                if self.search_tree(key, node.right):
+                    return True
+                else:
+                    return False
+            else:
+                return False
+        else:
+            return False
+# Order of results may vary
+bag = tree()
+bag.insert("sword")
+bag.insert("flask")
+bag.insert("flying_boots")
+
+for x in bag:
+    print(x) # should show flask, sword, flying_boots
+
+print("--------------------")
+
+bag.insert("shield")
+bag.insert("ale")
+bag.insert("plate_armor")
+bag.insert("purse")
+
+for x in reversed(bag):
+    print(x)
+    # should have flying_boots, purse, shield
+    # plate_armor, flask, ale, sword
+
+print("--------------------")
+
+print(bag.search_tree(hash("water_bag"), bag.root)) # should return false
+print(bag.search_tree(hash("shield"), bag.root)) # should return true
